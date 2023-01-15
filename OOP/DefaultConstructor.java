34ca7e5c7465d2be610e0bf6a50f69db2570656b
S.Boopathi <119664250+Boopathi5807@users.noreply.github.com>
2023-01-15 10:27:41 +0530

    Add files via upload

---
+++ OOP/DefaultConstructor.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class DefaultConstructor {
+	DefaultConstructor() // DeFault Constructor using Name of Class Name
+	{
+		System.out.println("excution of Default Constructor....");
+	}
+
+	// Main Method
+	public static void main(String[] args) {
+		// calling the Default Constructor
+		DefaultConstructor obj = new DefaultConstructor();
+
+	}
+
+}
---
+++ OOP/DisplayMethod.java
@@ -0,0 +1,28 @@
+package oop;
+
+//using Display Method TO Display An Value
+public class DisplayMethod {
+	int id;
+	String name;
+
+	public DisplayMethod(int id, String name)// Parameterized Constructor
+	{
+		this.id = id;
+		this.name = name;
+		System.out.println("ID:NAME");
+	}
+
+	void Display()// Display Method
+	{
+		System.out.println(id + "" + name);
+	}
+
+	public static void main(String[] args) {
+		// calling Constructor
+		DisplayMethod obj = new DisplayMethod(1, "\nBoopathi");
+		// calling Display Method And asign to Display the Value of Constructor
+		obj.Display();
+
+	}
+
+}
